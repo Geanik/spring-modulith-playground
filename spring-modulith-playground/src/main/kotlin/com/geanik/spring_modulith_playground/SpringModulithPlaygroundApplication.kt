85ac06f95ec4838a29d2021a5687c3148bd20b22
Jan Wiesbauer <Geanik@users.noreply.github.com>
2024-02-16 18:48:42 +0100

    Spring project setup

---
+++ spring-modulith-playground/src/main/kotlin/com/geanik/spring_modulith_playground/SpringModulithPlaygroundApplication.kt
@@ -0,0 +1,11 @@
+package com.geanik.spring_modulith_playground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringModulithPlaygroundApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringModulithPlaygroundApplication>(*args)
+}
